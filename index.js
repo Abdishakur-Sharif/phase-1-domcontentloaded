1838b78cc4d3a14bdb99e6c739a070f662f451da
xhaqur xhariph <shakuritoshariff@gmail.com>
2024-07-05 15:43:42 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("this has loaded")
+    const paragraph = document.querySelector("p")
+    paragraph.textContent = "This is really cool!"
+})
+
